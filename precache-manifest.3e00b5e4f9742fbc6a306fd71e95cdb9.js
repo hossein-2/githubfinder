9fd0a48ed2e2a0d6d6ded6d1636e1a59f965e70e
hossein tabatabaei <hosseintabatabaei@yahoo.com>
2020-01-26 19:07:31 +0330

    Updates

---
+++ precache-manifest.3e00b5e4f9742fbc6a306fd71e95cdb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f828749e244f2a7f37e4266c299ce6",
+    "url": "/githubfinder/index.html"
+  },
+  {
+    "revision": "3d6b396950d7bcb1135d",
+    "url": "/githubfinder/static/css/main.39491247.chunk.css"
+  },
+  {
+    "revision": "e5f4a1671a35300d7b8d",
+    "url": "/githubfinder/static/js/2.07f3064a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/githubfinder/static/js/2.07f3064a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d6b396950d7bcb1135d",
+    "url": "/githubfinder/static/js/main.0b5b4df4.chunk.js"
+  },
+  {
+    "revision": "cf3c78ec5a43ed56b612",
+    "url": "/githubfinder/static/js/runtime-main.d18dbc0c.js"
+  },
+  {
+    "revision": "e478e3f69e8a8d9096587eca67d76628",
+    "url": "/githubfinder/static/media/rish.e478e3f6.gif"
+  }
+]);
